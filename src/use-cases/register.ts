ac0073357dbd9582517c07adfe7768b310c8da2c
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-27 21:13:43 -0300

    aplicando praticas SOLID

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { hash } from "bcryptjs";
+import { UsersRepository } from "@/repositories/user-repository";
+import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -7,28 +9,25 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6);
+// SOLID - 5 PRINCIPIOS
+// D - Dependenncy Inversion Principle
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  if (userWithSameEmail) {
-    throw new Error("E-mail already exists.");
-  }
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
 
-  await prisma.user.create({
-    data: {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    await this.usersRepository.create({
       name,
       email,
       password_hash,
-    },
-  });
+    });
+  }
 }
